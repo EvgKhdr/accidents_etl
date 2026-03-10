1ed052f304887e63ca65a1313d6de1c7da328124
Evgenei Khodar <evg.khdr@gmail.com>
2026-03-10 10:05:28 +0100

    corrected CHECK constraints in db schema

--- db_creation.sql
+++ db_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE accidents(
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL,
     date TIMESTAMPTZ NOT NULL,
-    severity VARCHAR(10) CHECK (severity IN ('Fatal', 'Slight', 'Serious')) DEFAULT 'Unknown',
+    severity VARCHAR(10) CHECK (severity IN ('Unknown','Fatal', 'Slight', 'Serious')) DEFAULT 'Unknown',
     borough VARCHAR(30) CHECK (borough IN (
         'Brent', 'Redbridge', 'Waltham Forest', 'Lewisham', 'Wandsworth', 
         'Barnet', 'Southwark', 'Bexley', 'Lambeth', 'Richmond upon Thames', 
@@ -13,7 +13,7 @@ CREATE TABLE accidents(
         'Kensington and Chelsea', 'Camden', 'Islington', 'City of London', 
         'Havering', 'Hammersmith and Fulham', 'Newham', 'Harrow', 'Merton', 
         'Bromley', 'Hackney', 'Hillingdon', 'Ealing', 'Greenwich', 'Kingston', 
-        'Barking and Dagenham', 'Croydon', 'Haringey'
+        'Barking and Dagenham', 'Croydon', 'Haringey', 'Unknown'
     )) DEFAULT 'Unknown'
 );
 
@@ -21,13 +21,13 @@ CREATE TABLE casualties(
     id SERIAL PRIMARY KEY,
     accident_id INT REFERENCES accidents (id),
     age INT DEFAULT NULL,
-    class VARCHAR(10) CHECK (class IN ('Driver', 'Pedestrian', 'Passenger')) DEFAULT 'Unknown',
+    class VARCHAR(10) CHECK (class IN ('Driver', 'Pedestrian', 'Passenger', 'Unknown')) DEFAULT 'Unknown',
     mode VARCHAR(20) CHECK (mode IN (
         'PrivateHire', 'Taxi', 'GoodsVehicle', 'Car', 'BusOrCoach', 
-        'Pedestrian', 'PoweredTwoWheeler', 'OtherVehicle', 'PedalCycle'
+        'Pedestrian', 'PoweredTwoWheeler', 'OtherVehicle', 'PedalCycle', 'Unknown'
     )) DEFAULT 'Unknown',
-    severity VARCHAR(10) CHECK (severity IN ('Fatal', 'Slight', 'Serious')) DEFAULT 'Unknown',
-    age_band VARCHAR (10) CHECK (age_band IN ('Child', 'Adult')) DEFAULT 'Unknown'
+    severity VARCHAR(10) CHECK (severity IN ('Fatal', 'Slight', 'Serious','Unknown')) DEFAULT 'Unknown',
+    age_band VARCHAR (10) CHECK (age_band IN ('Child', 'Adult', 'Unknown')) DEFAULT 'Unknown'
 );
 
 CREATE TABLE vehicles(
